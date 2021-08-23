84458c4b2763c6d70bd01663eca7b3aa4ffe6c51
Sharmin Afifa <79069759+sharminafifa25@users.noreply.github.com>
2021-08-22 22:14:39 -0500

    payment front-end

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 from .views import *
 
 urlpatterns = [
-    path('' ,home ,name="home")
+    path('' ,home ,name="home"),
+    path('payment/', views.payment ,name="payment"),
 ]
