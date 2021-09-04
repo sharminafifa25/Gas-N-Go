3bcb9163ae2d31ad80a7ffc8ac065cde86e3daf1
Sharmin Afifa <79069759+sharminafifa25@users.noreply.github.com>
2021-09-03 22:42:44 -0500

    updated login.html

---
+++ acccounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from .views import *
+
+urlpatterns = [
+    path('', login, name="login"),
+  
+   
+]
--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('' ,include('home.urls')),
+    path('', include('home.urls')),
+    path('accounts/', include('acccounts.urls')),
     path('admin/', admin.site.urls),
 ]
